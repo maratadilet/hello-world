eb92e443613f9aff192af04795b669644f2ae779
maratadilet <141257607+maratadilet@users.noreply.github.com>
2023-08-02 11:16:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = var.ami_id
   instance_type          = var.instance_type
   availability_zone      = var.az1
   key_name               = var.key_pair
