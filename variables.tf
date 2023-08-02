7bdfb1b6a42569ec69de049731f9379b35cd5b0c
maratadilet <141257607+maratadilet@users.noreply.github.com>
2023-08-02 11:17:00 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,12 +27,3 @@ variable region {
   default     = ""
   description = "Proved region"
 }
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = [var.ami_name]
-  }
-  owners = ["self"]
-}
